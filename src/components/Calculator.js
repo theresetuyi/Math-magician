4076347071d2ade537f01b03fb8334ce5ea11ce5
unknown <teddytiyi@gmail.com>
2023-05-31 01:15:16 +0200

    add components

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Calculator() {
+  return (
+    <form action="#">
+      <input type="text" placeholder="0" className="number" />
+      <div className="calc-btn">
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="sign">:</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="sign">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="sign">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="sign">+</button>
+        <button type="button" className="zero">0</button>
+        <button type="button">.</button>
+        <button type="button" className="sign">=</button>
+
+      </div>
+    </form>
+  );
+}
+
+export default Calculator;
